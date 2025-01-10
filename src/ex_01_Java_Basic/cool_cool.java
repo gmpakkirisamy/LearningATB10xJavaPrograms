28506b4eea339e8ea876aac8ff69259e1a318be4
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-01-10 19:33:13 +0530

    10th Jan_3

--- src/ex_01_Java_Basic/cool_cool.java
+++
@@ -1,7 +0,0 @@
-package ex_01_Java_Basic;
-
-public class cool_cool {
-    public static void main(String[] args) {
-        System.out.println("Love");
-    }
-}
--- src/ex_01_Java_Basic/newclaass.java
+++
@@ -1,8 +0,0 @@
-package ex_01_Java_Basic;
-
-public class newclaass {
-    public static void main(String[] args) {
-        System.out.println("hi");
-        /// gklafjasdfhds
-    }
-}
