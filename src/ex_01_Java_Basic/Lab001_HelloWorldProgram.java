af8cdc98a8ce9c1f65b183b633f001a9631a4f72
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-01-10 18:51:21 +0530

    10th Jan

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) { // type main and enter as shout cut
         System.out.println("Hello Da"); // type sout and enter as short cut
+
     }
 }
