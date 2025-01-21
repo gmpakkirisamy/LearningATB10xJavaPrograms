0f02d0834c7e355769c50818704e0e7c531bd0ac
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-01-21 18:21:50 +0530

    21-Jan_3

---
+++ src/ex_04_TypeCasting/Lab_13_TypeCasting.java
@@ -0,0 +1,19 @@
+package ex_04_TypeCasting;
+
+public class Lab_13_TypeCasting {
+    public static void main(String[] args) {
+
+        //Widening
+        byte b = 10;
+       // int a= b;   // Valid case --> Implicit widening - Store the byte value into integer
+        int a= (int)b;   // Valid case --> Explicit widening
+
+        //Narrowing
+
+        int d = 200;
+        // byte e = d; //Invalid for implicit casting narrowing is not possible
+        byte e = (byte)d; // Valid -> Explicit casting - Allowed but loss of data will be there.
+
+
+    }
+}
