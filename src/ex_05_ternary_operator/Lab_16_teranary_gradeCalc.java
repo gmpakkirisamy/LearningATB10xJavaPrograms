1de46d7ff79c70c792269404b1eb777fec69fcab
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-01-25 09:00:31 +0530

    25-Jan_1

--- src/ex_05_ternary_operator/Lab_16_teranary_gradeCalc.java
+++ src/ex_05_ternary_operator/Lab_16_teranary_gradeCalc.java
@@ -1,4 +1,10 @@
 package ex_05_ternary_operator;
 
 public class Lab_16_teranary_gradeCalc {
+    public static void main(String[] args) {
+        int score = 69;
+
+        String result = (score<=59 ? "Your Grade is F" : (score>=90 ? "Your Grade is A" : (score>=80 ? "Your Grade is B" :(score>=70 ? "Your Grade is C " : "Your Grade is D"))));
+        System.out.println(result);
+    }
 }
