dec67f351b4e1fa20b11eb10464eb5de1299e878
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-01-29 12:01:05 +0530

    25-Jan_1

---
+++ src/ex_09_Switch_statement/Lab_38_multiple_match.java
@@ -0,0 +1,17 @@
+package ex_09_Switch_statement;
+
+public class Lab_38_multiple_match {
+    public static void main(String[] args) {
+        //JDK>13 support only when JDK is more than 13
+        int itemcode = 006;
+
+        switch (itemcode) {
+            case 001,002,003:
+                System.out.println("All items are Fruit");
+                break;
+            case 004,005,006:
+                System.out.println("All items are Electronic");
+                break;
+        }
+    }
+}
---
+++ src/ex_10_for_loop/Lab_40_for_loop.java
@@ -0,0 +1,10 @@
+package ex_10_for_loop;
+
+public class Lab_40_for_loop {
+    public static void main(String[] args) {
+        //Condition should be there, otherwise program will nun endlessly.
+        for(int i =1;i<=100;i++) {
+            System.out.println(i);
+        }
+    }
+}
