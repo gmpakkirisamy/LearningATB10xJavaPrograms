6faf74645b76a749c3de792125a2ed99cf3cd244
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-01-31 21:22:55 +0530

    31-Jan

---
+++ src/ex_15_functions/Lab_56_functions_basics.java
@@ -0,0 +1,32 @@
+package ex_15_functions;
+
+public class Lab_56_functions_basics {
+    public static void main(String[] args) {
+     //FUNCTIONS
+        // Functions often referred to as methods in java
+        // Its the java block of code designed to perform specific task
+
+        //KEY COMPONENTS OF JAVA FUNCTIONS
+            //A function must be declared within the class
+                //Syntex//public returnType functionName  (parameterType parameterName){
+                // body of the function
+                //}
+
+            //Calling - Until you cal the function , you can't run it.
+                //Syntex - functionName(arguments);
+
+
+        //Two Type of Functions
+            //1. Build in functions or pre defined eg. main function is called by JVM
+            //2. User defined functions
+
+        //USER DEFINED FUNCTIONS
+            //Four Types
+        //Without Parameter and without retrun type
+        //Without Parameter and with retrun type
+        //With Parameter and without retrun type
+        //With Parameter and without retrun type
+
+
+    }
+}
