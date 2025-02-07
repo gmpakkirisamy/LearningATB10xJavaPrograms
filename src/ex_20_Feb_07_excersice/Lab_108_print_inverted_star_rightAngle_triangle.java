3d994400b45a7c5c69cc7d5dd352885abcb392e7
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-02-07 21:26:07 +0530

    07-Feb_2

---
+++ src/ex_20_Feb_07_excersice/Lab_108_print_inverted_star_rightAngle_triangle.java
@@ -0,0 +1,12 @@
+package ex_20_Feb_07_excersice;
+
+public class Lab_108_print_inverted_star_rightAngle_triangle {
+    public static void main(String[] args) {
+        for (int i = 5; i >0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
