c2d73830eaa017edf6fa7c07a4572bc9f2263b95
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-02-11 09:32:17 +0530

    11-Feb

---
+++ src/ex_23_OOPS_class_and_objects/Lab_128_class_example.java
@@ -0,0 +1,23 @@
+package ex_23_OOPS_class_and_objects;
+
+public class Lab_128_class_example {
+
+    // This is the class which not cosume the space untill it was called.
+
+    //Attributes
+
+    String name;
+    int age;
+    String gender;
+    long phone;
+    int salary;
+    String address;
+
+
+    //Behaviour
+    void sleep(){};
+    void eat(){};
+    void speak(){};
+
+
+}
---
+++ src/ex_23_OOPS_constructors/Lab_131_Constructors.java
@@ -0,0 +1,35 @@
+package ex_23_OOPS_constructors;
+
+public class Lab_131_Constructors {
+    public static void main(String[] args) {
+
+        baby b1 = new baby();
+        b1.name = "Samy";
+
+        new baby(); // Automatically this will be called
+        new baby(); // Automatically this will be called
+
+
+
+    }
+
+    }
+        class baby {
+        String name;
+
+        // This is default constructor
+        baby(){
+            System.out.println("Am called.........");
+            //Constructors - It is the special method which is invoked automatically at the time of object creation
+            //Once the object is created immediately. Name will be created after the object is creted.
+            //This should not have any access modifier to be typed like void/private/public
+            //Consturctor name should be same as the method and should not have retun type - THis is condition.
+            //And this will be automatically called when the object is created.
+            // IIB block also will be called when object initialization, we can use if it required otherwise its not needed.
+
+        }
+
+            {  //IIB - Instance Initialization Block
+                System.out.println("Am also called, when class is loaded Am IIB!");
+            }
+}
