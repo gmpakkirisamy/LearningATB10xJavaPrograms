6357c6e723fb141db84c52199a5c227b12b18324
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-02-14 12:35:01 +0530

    14-Feb

---
+++ src/ex_25_SelfExcersice/Lab_146_convert_string_array.java
@@ -0,0 +1,13 @@
+package ex_25_SelfExcersice;
+
+public class Lab_146_convert_string_array {
+    public static void main(String[] args) {
+        String myStr = "Pakkirisamy";
+
+        char[] myArray = myStr.toCharArray();
+
+        for(char i : myArray){
+        System.out.println(i);
+        }
+    }
+}
---
+++ src/ex_25_SelfExcersice/Lab_147_sort_an_array.java
@@ -0,0 +1,25 @@
+package ex_25_SelfExcersice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lab_147_sort_an_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter how many word you want to sort ?");
+        int tword = sc.nextInt();
+
+        String[] words = new String[tword]; // Initialize the value to the array
+        System.out.println("Enter the words one by one to sort!");
+        for (int i = 0; i < words.length; i++) {
+
+            words[i] = sc.next();
+        }
+        Arrays.sort(words); // This will sort function for arrays.
+
+        for (String i : words) {
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
+}
