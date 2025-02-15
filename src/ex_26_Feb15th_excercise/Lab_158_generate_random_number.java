72ebe3cbd6ffb31982b25ab91de211e5fd55eb6e
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-02-15 14:53:58 +0530

    15-Feb_Part_3

---
+++ src/ex_26_Feb15th_excercise/Lab_158_generate_random_number.java
@@ -0,0 +1,8 @@
+package ex_26_Feb15th_excercise;
+
+public class Lab_158_generate_random_number {
+    public static void main(String[] args) {
+        int random = (int) (Math.random()*101); // It will generate and print random number whenever it's executing this program.
+        System.out.println(random);
+    }
+}
