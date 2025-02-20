9b22f0da08123167ef2d4a63a12ea474eee4165c
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-02-20 18:35:44 +0530

    20-Feb_Part_1

---
+++ src/Java_OOPs/ex_37_Feb_19_Excersice/Lab_199_QandA.java
@@ -0,0 +1,36 @@
+package Java_OOPs.ex_37_Feb_19_Excersice;
+
+public class Lab_199_QandA {
+
+//1. What is a constructor in Java?
+//
+//    A constructor in Java is a special method used to initialize objects at the time of object creation. It is automatically invoked
+//    when an instance of a class is created.
+//
+//2. How is a constructor different from a method?
+//
+//    i) A constructor runs once when an object is created.
+//
+//    ii) A method can be called multiple times to perform actions.
+//
+//3.  What are the different types of constructors in Java?
+//
+//    i) Default Constructor: No arguments, assigns default values.
+//
+//    ii) Parameterized Constructor: Takes arguments for custom initialization.
+//
+//4. What is constructor overloading?
+//      class can have more than one constructor, each with different parameters.
+//      This allows objects of the class to be initialized in different ways.
+//
+//5. Can a constructor be final, static, or abstract?
+//   Constructors cannot be declared as final, static, or abstract
+//
+//6. Can we call a constructor explicitly inside another constructor?
+//   Yes, we can call a constructor explicitly using this() inside another constructor.
+//
+//7. What happens if you don’t define a constructor in a class?
+//     If no constructor is defined, Java provides a default constructor.
+//     The default constructor sets default values but performs no  logic.
+
+}
