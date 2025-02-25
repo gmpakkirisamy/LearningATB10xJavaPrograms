f204ead189b914867db6ef01ba923f57404165b7
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-02-25 19:52:16 +0530

    25-Feb_Part_4

---
+++ src/Java_Core_Concepts/Lab_228_Multiple_Exception.java
@@ -0,0 +1,19 @@
+package Java_Core_Concepts;
+
+public class Lab_228_Multiple_Exception {
+    public static void main(String[] args) {
+        //Here we have two types of exception. Arithmetic and NullPointer
+        int a = 0;
+        int b = 10;
+        try {
+
+            //int c = b / a;
+            String s1 = null;
+            s1.trim();
+        } catch (ArithmeticException | NullPointerException e) { // You can also handle the exception using the or method.
+            System.out.println(e.getMessage());
+
+        }
+        System.out.println(b);
+    }
+}
